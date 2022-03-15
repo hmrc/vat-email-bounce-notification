810bcadd405b166f860ff718c62412eaca0ea1d2
Lewis Hay <29063729+lewishay@users.noreply.github.com>
2022-03-15 12:15:18 +0000

    Added dummy connector and API response model

---
+++ app/connectors/UpdateContactPrefConnector.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import models.UpdateContactPrefResponse
+
+import javax.inject.Singleton
+import scala.concurrent.Future
+
+@Singleton
+class UpdateContactPrefConnector {
+
+  def updateContactPref(): Future[UpdateContactPrefResponse] =
+    Future.successful(UpdateContactPrefResponse("2020-01-01T09:00:00Z", "OK"))
+}
