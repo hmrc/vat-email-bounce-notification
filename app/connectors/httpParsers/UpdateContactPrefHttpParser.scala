7e285e34d24e1d152d3f94b1301fb288e84117e4
Lewis Hay <29063729+lewishay@users.noreply.github.com>
2022-03-31 14:05:19 +0100

    Added HTTP parser for update contact pref call

---
+++ app/connectors/httpParsers/UpdateContactPrefHttpParser.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.httpParsers
+
+import models.UpdateContactPrefResponse
+import play.api.http.Status.OK
+import uk.gov.hmrc.http.{HttpReads, HttpResponse}
+import utils.LoggerUtil
+
+object UpdateContactPrefHttpParser extends LoggerUtil {
+
+  implicit object UpdateContactPrefReads extends HttpReads[Option[UpdateContactPrefResponse]] {
+    override def read(method: String, url: String, response: HttpResponse): Option[UpdateContactPrefResponse] =
+      response.status match {
+        case OK =>
+          response.json.validate[UpdateContactPrefResponse].fold(
+            invalid => {
+              logger.warn(s"[UpdateContactPrefReads][read] - Invalid JSON returned by EIS. Validation error: $invalid")
+              None
+            },
+            valid => {
+              logger.debug(s"[UpdateContactPrefReads][read] - Valid JSON received: ${response.json}")
+              Some(valid)
+            }
+          )
+        case _ =>
+          logger.warn(s"[UpdateContactPrefReads][read] - Unexpected response received from EIS. " +
+            s"Status: ${response.status}, Body: ${response.body}")
+          None
+      }
+  }
+}
---
+++ test/connectors/httpParsers/UpdateContactPrefHttpParserSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.httpParsers
+
+import models.UpdateContactPrefResponse
+import play.api.http.Status
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.HttpResponse
+import utils.TestUtil
+
+class UpdateContactPrefHttpParserSpec extends TestUtil {
+
+  "The UpdateContactPrefHttpParser" when {
+
+    "a valid JSON body is returned by the API" should {
+
+      "return the response model" in {
+        val response = HttpResponse(Status.OK, json = Json.obj("processingDate" -> "2022-02-02", "status" -> "OK"), Map.empty)
+        val result = UpdateContactPrefHttpParser.UpdateContactPrefReads.read("POST", "/", response)
+        result shouldBe Some(UpdateContactPrefResponse("2022-02-02", "OK"))
+      }
+    }
+
+    "an invalid JSON body is returned by the API" should {
+
+      "return None" in {
+        val response = HttpResponse(Status.OK, json = Json.obj("dinner" -> "pizza"), Map.empty)
+        val result = UpdateContactPrefHttpParser.UpdateContactPrefReads.read("POST", "/", response)
+        result shouldBe None
+      }
+    }
+
+    "a non-200 response is returned by the API" should {
+
+      "return None" in {
+        val response = HttpResponse(Status.INTERNAL_SERVER_ERROR, "")
+        val result = UpdateContactPrefHttpParser.UpdateContactPrefReads.read("POST", "/", response)
+        result shouldBe None
+      }
+    }
+  }
+}
